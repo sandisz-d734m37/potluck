e54a72c30c8d7981fa8193617cb68ce89650f2e1
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-08 21:00:00 -0700

    Feat: Implement and test get all from category method to the Potluck class

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -11,4 +11,16 @@ class Potluck
   def add_dish(new_dish)
     dishes << new_dish
   end
+
+
+  def get_all_from_category(category)
+    all_from_category = []
+
+    dishes.each do |dish|
+      if dish.category == category
+        all_from_category << dish
+      end
+    end
+    all_from_category
+  end
 end
